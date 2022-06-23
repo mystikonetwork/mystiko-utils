565cf8184cc798ca4d803e1cbde9413af40ca01a
Fitch Li <fitch@mystiko.network>
2022-06-23 02:25:28 -0700

    fix potential memory leak of promiseWithTimeout

--- packages/utils/src/promise.ts
+++ packages/utils/src/promise.ts
@@ -1,8 +1,14 @@
 export class TimeoutError extends Error {}
 
 export function promiseWithTimeout<T>(promise: Promise<T>, timeoutMs: number): Promise<T> {
+  let timer: NodeJS.Timer | undefined;
   const timeoutPromise = new Promise((resolve) => {
-    setTimeout(resolve, timeoutMs);
+    timer = setTimeout(resolve, timeoutMs);
   }).then(() => Promise.reject(new TimeoutError(`timeout after ${timeoutMs} ms`)));
-  return Promise.race([promise, timeoutPromise]);
+  return Promise.race([promise, timeoutPromise]).finally(() => {
+    if (timer) {
+      clearTimeout(timer);
+      timer = undefined;
+    }
+  });
 }
