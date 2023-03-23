ace245d88351a14cd66f7509bb615ac2959a51d0
Fitch Li <fitch@mystiko.network>
2023-03-23 14:35:34 +0800

    add country code detector

---
+++ packages/utils/tests/detect.test.ts
@@ -0,0 +1,20 @@
+import nock from 'nock';
+import { DEFAULT_IP_API, detectCountryCode } from '../src';
+
+test('test detectCountryCode', async () => {
+  nock(DEFAULT_IP_API).get('/').reply(200, { country_code: 'CN' });
+  const country = await detectCountryCode();
+  expect(country).toBe('CN');
+});
+
+test('test detectCountryCode raise error', async () => {
+  nock(DEFAULT_IP_API).get('/').reply(500, {});
+  await expect(detectCountryCode()).rejects.toThrow();
+});
+
+test('test detectCountryCode different api', async () => {
+  const url = 'https://ipinfo.io';
+  nock(url).get('/').reply(200, { country_code: 'CN' });
+  const country = await detectCountryCode(url);
+  expect(country).toBe('CN');
+});
