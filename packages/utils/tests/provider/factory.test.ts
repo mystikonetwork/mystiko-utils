4a13529c2ab12125186d3809346c7fdc2100d41d
Fitch Li <fitch@mystiko.network>
2023-04-14 13:52:46 +0800

    add QuorumProviderFactory for creating quorum providers

--- packages/utils/tests/provider/factory.test.ts
+++ packages/utils/tests/provider/factory.test.ts
@@ -1,5 +1,10 @@
 import { ethers } from 'ethers';
-import { DefaultProviderFactory, FallbackProvider, ReconnectingWebSocketProvider } from '../../src';
+import {
+  DefaultProviderFactory,
+  FallbackProvider,
+  QuorumProviderFactory,
+  ReconnectingWebSocketProvider,
+} from '../../src';
 import { buildServer, closeServer } from './websocket-server';
 
 test('test DefaultProviderFactory', async () => {
@@ -23,4 +28,42 @@ test('test DefaultProviderFactory', async () => {
   await ((provider3 as FallbackProvider).providers[0] as ReconnectingWebSocketProvider).destroy();
   expect(() => factory.createProvider(['file://abc.socket'])).toThrow();
   await closeServer(wsServer);
+});
+
+test('test QuorumProviderFactory', () => {
+  const factory = new QuorumProviderFactory();
+  const provider1 = factory.createProvider(['http://localhost:39740']);
+  expect((provider1 as ethers.providers.FallbackProvider).quorum).toBe(1);
+  expect((provider1 as ethers.providers.FallbackProvider).providerConfigs[0].weight).toBe(1);
+  const provider2 = factory.createProvider(
+    [
+      'http://localhost:39740',
+      {
+        url: 'http://localhost:8088',
+      },
+      {
+        url: 'http://localhost:9088',
+        quorumWeight: 2,
+      },
+    ],
+    {},
+  );
+  expect((provider2 as ethers.providers.FallbackProvider).quorum).toBe(2);
+  expect((provider2 as ethers.providers.FallbackProvider).providerConfigs[0].weight).toBe(1);
+  expect((provider2 as ethers.providers.FallbackProvider).providerConfigs[1].weight).toBe(1);
+  expect((provider2 as ethers.providers.FallbackProvider).providerConfigs[2].weight).toBe(2);
+  const provider3 = factory.createProvider(
+    [
+      'http://localhost:39740',
+      {
+        url: 'http://localhost:8088',
+      },
+      {
+        url: 'http://localhost:9088',
+        quorumWeight: 2,
+      },
+    ],
+    { quorumPercentage: 70 },
+  );
+  expect((provider3 as ethers.providers.FallbackProvider).quorum).toBe(3);
 });
