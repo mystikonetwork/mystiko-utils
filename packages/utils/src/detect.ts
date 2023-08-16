e7ab67fdc37c1915e3032149e3aea282d051feb3
Fitch Li <fitch@mystiko.network>
2023-08-16 19:39:01 +0800

    add detectConcurrency function (#19)

--- packages/utils/src/detect.ts
+++ packages/utils/src/detect.ts
@@ -6,3 +6,15 @@ export function detectCountryCode(api?: string): Promise<string | undefined> {
   const apiUrl = api || DEFAULT_IP_API;
   return axios.get(apiUrl).then((resp) => resp.data.country_code);
 }
+
+export function detectConcurrency(): number | undefined {
+  if (typeof window !== 'undefined' && window.navigator && 'hardwareConcurrency' in window.navigator) {
+    return window.navigator.hardwareConcurrency;
+  }
+  if (typeof process !== 'undefined') {
+    // eslint-disable-next-line global-require
+    const os = require('os') as typeof import('os'); // Dynamically require 'os' only in a Node.js environment
+    return os.cpus().length;
+  }
+  return undefined;
+}
